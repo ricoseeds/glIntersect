a8f0d61e9db736b173e4586295cb8133a776df0a
ricoseeds <rghchaks73@gmail.com>
2019-03-03 14:02:06 +0530

    Add Poly on top of tex

--- src/main.cpp
+++ src/main.cpp
@@ -13,19 +13,21 @@ int main(int argc, char *argv[])
         return -1;
     }
     //Load the Shaders
-    ShaderProgram lightingShader;
+    ShaderProgram lightingShader, polyShader;
     lightingShader.loadShaders("shaders/vertx.vert", "shaders/fragh.frag");
+    polyShader.loadShaders("shaders/poly.vert", "shaders/poly.frag");
     Texture2D texture;
     texture.loadTexture("images/ducktomy.png", false);
     //Vertices for image
 
-    GLfloat symmetry_create = 0.8; //max is 1 for entire screen
-    // static const GLfloat image_points[] = {
-    //     -symmetry_create, -symmetry_create, 0,
-    //     symmetry_create, -symmetry_create, 0,
-    //     symmetry_create, symmetry_create, 0,
-    //     // 0, symmetry_create - 0.2, 0,
-    //     -symmetry_create, symmetry_create, 0};
+    GLfloat symmetry_create = 1.0;  //max is 1 for entire screen
+    GLfloat symmetry_create1 = 0.4; //max is 1 for entire screen
+    static const GLfloat image_points[] = {
+        -symmetry_create1, -symmetry_create1, 0,
+        symmetry_create1, -symmetry_create1, 0,
+        symmetry_create1, symmetry_create1, 0,
+        // 0, symmetry_create - 0.2, 0,
+        -symmetry_create1, symmetry_create1, 0};
     static const GLfloat verties[] = {
         -symmetry_create,
         symmetry_create,
@@ -72,6 +74,12 @@ int main(int argc, char *argv[])
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indeces), indeces, GL_STATIC_DRAW);
 
+    // Enable blending
+    glEnable(GL_BLEND);
+    // glBlendFunc(0.5, 0.5);
+    // glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    // glBlendEquationSeparate(GL_FUNC_ADD, GL_FUNC_ADD);
+    glBlendFuncSeparate(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_ONE, GL_ZERO);
     // Game loop
     while (!glfwWindowShouldClose(gWindow))
     {
@@ -80,27 +88,27 @@ int main(int argc, char *argv[])
         // Clear the screen
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         // Rest of the stuff
-        lightingShader.use();
-        //code
-        // GLuint mVBO, mVAO;
+        // lightingShader.use();
+        polyShader.use();
+        //Dynamic part involes drawing polygon
+        GLuint mVBO, mVAO;
+        glGenVertexArrays(1, &mVAO);
+        glGenBuffers(1, &mVBO);
+        glBindVertexArray(mVAO);
+        glBindBuffer(GL_ARRAY_BUFFER, mVBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(image_points), image_points, GL_STATIC_DRAW);
+        // Vertex Positions
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+        glEnableVertexAttribArray(0);
+        glBindVertexArray(0);
+        //actual rendering of poly
+        glBindVertexArray(mVAO);
+        glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+        glBindVertexArray(0);
+        //End Dynamic part involes drawing polygon
 
-        // glGenVertexArrays(1, &mVAO);
-
-        // glGenBuffers(1, &mVBO);
-
-        // glBindVertexArray(mVAO);
-        // glBindBuffer(GL_ARRAY_BUFFER, mVBO);
-        // glBufferData(GL_ARRAY_BUFFER, sizeof(image_points), image_points, GL_STATIC_DRAW);
-
-        // // Vertex Positions
-        // glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
-        // glEnableVertexAttribArray(0);
-        // glBindVertexArray(0);
-        // //code
-        // glBindVertexArray(mVAO);
-        // glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
-        // glBindVertexArray(0);
         //Static draw part
+        lightingShader.use();
         texture.bind(0);
         glBindVertexArray(vao);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
